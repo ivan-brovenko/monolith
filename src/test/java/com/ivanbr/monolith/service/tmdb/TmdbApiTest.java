87fa8bc2496e111639bf7f7b808a1efe953f1c18
ivan-br <ivanbbbr@gmail.com>
2021-09-02 19:13:23 +0300

    MONOLITH - Add wiremock

--- src/test/java/com/ivanbr/monolith/service/tmdb/TmdbApiTest.java
+++ src/test/java/com/ivanbr/monolith/service/tmdb/TmdbApiTest.java
@@ -1,57 +1,54 @@
 package com.ivanbr.monolith.service.tmdb;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.ivanbr.monolith.entity.Actor;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
-
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class TmdbApiTest {
 
-    @MockBean
-    private RestTemplate restTemplateMock;
-
     @Autowired
     private TmdbApi tmdbApi;
 
+    @Rule
+    public WireMockRule wireMockRule =  new WireMockRule(8066);
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
-    public void testFindActorById() {
+    public void testFindActorById() throws JsonProcessingException {
+
+        Actor expectedActor = new Actor();
+        expectedActor.setId(1L);
+        expectedActor.setName("");
+        expectedActor.setDeathday("");
+        expectedActor.setBirthday("");
+
+        WireMock.stubFor(WireMock.get(WireMock.urlMatching("/person/(.+)"))
+                .willReturn(WireMock.ok()
+                        .withBody(objectMapper.writeValueAsString(expectedActor))
+                        .withHeader("Content-Type", "application/json")));
+
+
         Long actorId = 1L;
-        Actor expectedActor = mock(Actor.class);
-        ResponseEntity<Actor> responseEntityMock = mock(ResponseEntity.class);
 
-        when(responseEntityMock.getBody()).thenReturn(expectedActor);
-        when(restTemplateMock.exchange(
-                anyString(),
-                eq(HttpMethod.GET),
-                eq(null),
-                eq(new ParameterizedTypeReference<Actor>() {}))
-        ).thenReturn(responseEntityMock);
+        Actor actor = tmdbApi.findActorById(actorId);
 
-        Actor actualActor = tmdbApi.findActorById(actorId);
 
-        verify(restTemplateMock).exchange(
-                anyString(),
-                eq(HttpMethod.GET),
-                eq(null),
-                eq(new ParameterizedTypeReference<Actor>() {}));
-        assertEquals(expectedActor, actualActor);
+        WireMock.verify(WireMock.getRequestedFor(WireMock.urlPathMatching("/person/1")));
+        assertEquals(expectedActor, actor);
     }
 }
